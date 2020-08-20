e6f5096ac054088ad2c84dd9cc41e00b4b7d9a6d
Quang Dang <quangdangfit@gmail.com>
2020-08-20 07:33:32 +0700

    update load auth option

--- app/auth.go
+++ app/auth.go
@@ -10,25 +10,31 @@ import (
 
 func InitAuth() (jwtAuth.IJWTAuth, error) {
 
-	cfg := config.Config.JWTAuth
+	conf := config.Config.JWTAuth
 	var opts []jwtAuth.Option
 	//access token
 	opts = append(opts, jwtAuth.WithKeyFunc(func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, errors.ErrTokenInvalid
 		}
-		return []byte(cfg.SigningKey), nil
+		return []byte(conf.SigningKey), nil
 	}))
-	opts = append(opts, jwtAuth.WithExpired(cfg.Expired))
-	opts = append(opts, jwtAuth.WithSigningKey([]byte(cfg.SigningKey)))
+	if conf.Expired != 0 {
+		opts = append(opts, jwtAuth.WithExpired(conf.Expired))
+	}
+	opts = append(opts, jwtAuth.WithSigningKey([]byte(conf.SigningKey)))
+
 	//refresh token
 	opts = append(opts, jwtAuth.WithKeyFuncRefresh(func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, errors.ErrTokenInvalid
 		}
-		return []byte(cfg.SigningRefreshKey), nil
+		return []byte(conf.SigningRefreshKey), nil
 	}))
-	opts = append(opts, jwtAuth.WithExpiredRefresh(cfg.ExpiredRefreshToken))
-	opts = append(opts, jwtAuth.WithSigningKeyRefresh([]byte(cfg.SigningRefreshKey)))
+
+	if conf.ExpiredRefreshToken != 0 {
+		opts = append(opts, jwtAuth.WithExpiredRefresh(conf.ExpiredRefreshToken))
+	}
+	opts = append(opts, jwtAuth.WithSigningKeyRefresh([]byte(conf.SigningRefreshKey)))
 	return jwtAuth.NewJWTAuth(opts...), nil
 }
