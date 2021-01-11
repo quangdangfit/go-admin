2eb27bfb0843ace7ac844bd0c8d4b83080498505
Quang Dang <quangdangfit@gmail.com>
2021-01-11 10:30:33 +0700

    create new action

---
+++ .github/actions/test.sh
@@ -0,0 +1,11 @@
+git reset --hard
+git checkout develop
+git pull
+
+git config --global url.ssh://git@github.com/.insteadOf https://github.com/
+go get github.com/tokoinofficial/common_lib@develop
+go get github.com/tokoinofficial/error_lib@develop
+
+go mod vendor
+
+go test -v ./...
