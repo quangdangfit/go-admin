0fd8d4df33c93dd69ce18871695abe5b6143edcd
Quang Dang <quangdangfit@gmail.com>
2021-01-22 14:26:25 +0700

    update docker-compose.yml

--- config/config.go
+++ config/config.go
@@ -45,10 +45,12 @@ var Config Schema
 func init() {
 	config := viper.New()
 	config.SetConfigName("config")
-	config.AddConfigPath(".")          // Look for config in current directory
-	config.AddConfigPath("config/")    // Optionally look for config in the working directory.
-	config.AddConfigPath("../config/") // Look for config needed for tests.
-	config.AddConfigPath("../")        // Look for config needed for tests.
+	config.AddConfigPath(".")             // Look for config in current directory
+	config.AddConfigPath("config/")       // Optionally look for config in the working directory.
+	config.AddConfigPath("../config/")    // Look for config needed for tests.
+	config.AddConfigPath("../")           // Look for config needed for tests.
+	config.AddConfigPath("../../config/") // Look for config needed for tests.
+	config.AddConfigPath("../../")        // Look for config needed for tests.
 
 	config.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
 	config.AutomaticEnv()
