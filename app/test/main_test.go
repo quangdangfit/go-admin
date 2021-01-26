36815995da42d06ff6cb45536d513ac4c2ead1c6
Quang Dang <quangdangfit@gmail.com>
2021-01-26 15:10:46 +0700

    add test setup

---
+++ app/test/main_test.go
@@ -0,0 +1,208 @@
+package test
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/quangdangfit/gosdk/utils/logger"
+	"go.uber.org/dig"
+
+	"github.com/quangdangfit/go-admin/app"
+	"github.com/quangdangfit/go-admin/app/models"
+	"github.com/quangdangfit/go-admin/app/repositories"
+	"github.com/quangdangfit/go-admin/app/router"
+	"github.com/quangdangfit/go-admin/pkg/jwt"
+	"github.com/quangdangfit/go-admin/pkg/utils"
+)
+
+var (
+	engine    *gin.Engine
+	container *dig.Container
+	token     string
+
+	apiKey = "testAPIkey"
+
+	roles = []*models.Role{
+		{
+			Model: models.Model{
+				ID:        "test-role-id-1",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			Name:        "test1",
+			Description: "test1",
+		},
+	}
+
+	users = []*models.User{
+		{
+			Model: models.Model{
+				ID:        "test-user-id-1",
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			Username:     "test-username-1",
+			Email:        "testuseremail1@tokoin.io",
+			Password:     "test-user-pwd-1",
+			RefreshToken: "test-user-refresh-token-1",
+			RoleID:       roles[0].ID,
+		},
+	}
+
+	user = users[0]
+)
+
+func TestMain(m *testing.M) {
+	logger.Initialize(false)
+	container = app.BuildContainer()
+	engine = router.InitGinEngine(container)
+
+	setup()
+	code := m.Run()
+	teardown()
+
+	os.Exit(code)
+}
+
+func setup() {
+	logger.Info("============> Setup for testing")
+	removeUserData()
+
+	createRoleData()
+	createUserData()
+	setupToken()
+}
+
+func teardown() {
+	logger.Info("============> Teardown")
+	removeUserData()
+}
+
+func createRoleData() {
+	container.Invoke(func(
+		userRepo repositories.IRoleRepository,
+		jwtauth jwt.IJWTAuth,
+	) error {
+		for _, r := range roles {
+			if err := userRepo.Create(r); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+func createUserData() {
+	container.Invoke(func(
+		userRepo repositories.IUserRepository,
+		jwtauth jwt.IJWTAuth,
+	) error {
+		for _, u := range users {
+			hashed, err := utils.HashPassword([]byte(u.Password))
+			if err != nil {
+				return err
+			}
+
+			u.Password = string(hashed)
+			if err = userRepo.Create(u); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+func setupToken() {
+	container.Invoke(func(
+		jwtauth jwt.IJWTAuth,
+	) error {
+		tokenInfo, err := jwtauth.GenerateToken(user.ID)
+		if err != nil {
+			return err
+		}
+
+		token = tokenInfo.GetAccessToken()
+
+		return nil
+	})
+}
+
+func removeUserData() {
+	//container.Invoke(func(
+	//	userRepo repositories.IUserRepository,
+	//) error {
+	//	for _, u := range users {
+	//		err := userRepo.Delete(u.ID)
+	//		if err != nil {
+	//			return err
+	//		}
+	//	}
+	//	return nil
+	//})
+}
+
+//func clearRedis() {
+//	container.Invoke(func(
+//		redis redis.IRedis,
+//	) error {
+//		err := redis.Remove(utils.GenAPIKeyRedis("", apiKey))
+//		if err != nil {
+//			return err
+//		}
+//		return nil
+//	})
+//}
+
+func toReader(v interface{}) io.Reader {
+	buf := new(bytes.Buffer)
+	_ = json.NewEncoder(buf).Encode(v)
+	return buf
+}
+
+func parseReader(r io.Reader, v interface{}) error {
+	return json.NewDecoder(r).Decode(v)
+}
+
+func newGetRequest(formatRouter string, v interface{}, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("GET", fmt.Sprintf(formatRouter, args...), toReader(v))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	return req
+}
+
+func newPostRequest(formatRouter string, v interface{}, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("POST", fmt.Sprintf(formatRouter, args...), toReader(v))
+	return req
+}
+
+func newPostAuthRequest(formatRouter string, v interface{}, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("POST", fmt.Sprintf(formatRouter, args...), toReader(v))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	return req
+}
+
+func newInternalPostRequest(formatRouter string, v interface{}, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("POST", fmt.Sprintf(formatRouter, args...), toReader(v))
+	req.Header.Add("api-key", apiKey)
+	return req
+}
+
+func newPutRequest(formatRouter string, v interface{}, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("PUT", fmt.Sprintf(formatRouter, args...), toReader(v))
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	return req
+}
+
+func newDeleteRequest(formatRouter string, args ...interface{}) *http.Request {
+	req, _ := http.NewRequest("DELETE", fmt.Sprintf(formatRouter, args...), nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
+	return req
+}
